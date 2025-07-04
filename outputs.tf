a987331008975c1d8b193d61f45d5f47a1721c11
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-07-04 22:49:07 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "mautic_instance_name" {
-  description = "Mautic VM instance name"
-  value = google_compute_instance.mautic_vm.name
+output "jenkins_instance_name" {
+  description = "Jenkins VM instance name"
+  value = google_compute_instance.jenkins_vm.name
 }
 
 output "https_load_balancer_ip" {
   description = "HTTPS load balancer IP address"
-  value = google_compute_global_address.mautic_ip.address
+  value = google_compute_global_address.jenkins_ip.address
 }
 
 output "dns_configuration" {
   description = "DNS configuration needed"
-  value       = "Point ${var.domain_name} A record to ${google_compute_global_address.mautic_ip.address}"
+  value       = "Point ${var.domain_name} A record to ${google_compute_global_address.jenkins_ip.address}"
 }
