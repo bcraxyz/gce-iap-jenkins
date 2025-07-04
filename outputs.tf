50a27f7c32612f213f4613e5d6b15ad3990b4aab
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-07-04 20:45:50 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,10 @@ output "mautic_instance_name" {
 
 output "https_load_balancer_ip" {
   description = "HTTPS load balancer IP address"
-  value = google_compute_global_forwarding_rule.https_forwarding_rule.ip_address
+  value = google_compute_global_address.mautic_ip.address
+}
+
+output "dns_configuration" {
+  description = "DNS configuration needed"
+  value       = "Point ${var.domain_name} A record to ${google_compute_global_address.mautic_ip.address}"
 }
